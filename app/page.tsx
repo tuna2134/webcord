c9ad3b3e8897ce537800574bf7eb991ba6f1ed29
コマリン親衛隊 <masato190411@icloud.com>
2024-07-18 19:04:20 +0000

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,42 @@
+"use client";
+import { Form, FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  userid: z.bigint(),
+})
+
 export default function Home() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
   return (
-    <p>test</p>
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            control={form.control}
+            name="userid"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>ユーザーID</FormLabel>
+                <FormControl>
+                  <Input placeholder="ユーザーID" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <Button type="submit">検索</Button>
+        </form>
+      </Form>
+    </>
   );
 }
