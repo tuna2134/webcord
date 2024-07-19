d34509720012980861fccdab5a65514496298892
コマリン親衛隊 <masato190411@icloud.com>
2024-07-19 11:10:15 +0000

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental: {
-        serverActions: {
-            allowedOrigins: [
-                "special-umbrella-4ww7qwrqrp43qjvq.github.dev",
-                "localhost:3000",
-            ],
-        },
-    },
-};
+const nextConfig = {};
 
 export default nextConfig;
